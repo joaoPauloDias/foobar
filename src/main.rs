2b840471f70c4a0ca69a992aa15aea2cf6518c76
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2025-02-21 17:49:14 -0300

    barfoo init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
+use rand::{Rng, rng};
 
 #[derive(Component)]
 struct Foobar;
+
+#[derive(Component)]
+struct Barfoo;
 
 #[derive(Component)]
 struct Velocity(Vec2);
@@ -16,6 +20,19 @@ fn setup(mut commands: Commands) {
             ..Default::default()
         },
     ));
+
+    commands.spawn((
+        Barfoo,
+        Sprite::from_color(Color::srgb(0.0, 1.0, 0.0), Vec2::new(50.0, 50.0)),
+        Transform {
+            translation: Vec3::new(
+                rng().random_range(-500.0..500.0),
+                rng().random_range(-500.0..500.0),
+                1.0,
+            ),
+            ..Default::default()
+        },
+    ));
 }
 
 fn update_position(mut query: Query<(&Velocity, &mut Transform), With<Foobar>>, time: Res<Time>) {
@@ -24,10 +41,27 @@ fn update_position(mut query: Query<(&Velocity, &mut Transform), With<Foobar>>, 
     }
 }
 
+fn update_velocity(
+    mut query_foobar: Query<(&mut Velocity, &Transform), With<Foobar>>,
+    query_barfoo: Query<&Transform, With<Barfoo>>,
+) {
+    if let Ok(barfoo_transform) = query_barfoo.get_single() {
+        for (mut velocity, foobar_transform) in query_foobar.iter_mut() {
+            let direction =
+                (barfoo_transform.translation - foobar_transform.translation).truncate();
+            if direction.length() != 0.0 {
+                let normalized_direction = direction.normalize();
+                let speed = velocity.0.length();
+                velocity.0 = normalized_direction * speed;
+            }
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, update_position)
+        .add_systems(FixedUpdate, (update_position, update_velocity))
         .run();
 }
