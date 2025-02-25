db02a890e706551a9d79aeb70b29ec1169388cd4
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2025-02-24 19:13:54 -0600

    spawn barfoo with click

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use bevy::prelude::*;
-use rand::{Rng, rng};
-
 #[derive(Component)]
 struct Foobar;
 
@@ -8,52 +6,70 @@ struct Foobar;
 struct Barfoo;
 
 #[derive(Component)]
-struct Velocity(Vec2);
+struct Direction(Vec2);
+
+#[derive(Component)]
+struct Speed(f32);
+
+fn spawn_barfoo(mut commands: Commands, coords: Vec2) {
+    commands.spawn((
+        Barfoo,
+        Sprite::from_color(Color::srgb(0.0, 1.0, 0.0), Vec2::new(50.0, 50.0)),
+        Transform {
+            translation: coords.extend(-1.0),
+            ..Default::default()
+        },
+    ));
+}
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
     commands.spawn((
         Foobar,
-        Velocity(Vec2::new(100.0, 0.0)),
+        Direction(Vec2::ZERO),
+        Speed(1.5),
         Sprite::from_color(Color::srgb(1.0, 0.0, 0.0), Vec2::new(50.0, 50.0)),
         Transform {
             ..Default::default()
         },
     ));
-
-    commands.spawn((
-        Barfoo,
-        Sprite::from_color(Color::srgb(0.0, 1.0, 0.0), Vec2::new(50.0, 50.0)),
-        Transform {
-            translation: Vec3::new(
-                rng().random_range(-500.0..500.0),
-                rng().random_range(-500.0..500.0),
-                1.0,
-            ),
-            ..Default::default()
-        },
-    ));
 }
 
-fn update_position(mut query: Query<(&Velocity, &mut Transform), With<Foobar>>, time: Res<Time>) {
-    for (&Velocity(velocity), mut transform) in query.iter_mut() {
-        transform.translation += (velocity * time.delta_secs()).extend(0.0);
+fn update_position(mut query: Query<(&Direction, &Speed, &mut Transform)>) {
+    for (&Direction(direction), &Speed(speed), mut transform) in query.iter_mut() {
+        transform.translation += match direction.length() {
+            0.0 => Vec3::ZERO,
+            _ => (direction.normalize() * speed).extend(0.0),
+        };
     }
 }
 
-fn update_velocity(
-    mut query_foobar: Query<(&mut Velocity, &Transform), With<Foobar>>,
+fn update_direction(
+    mut query_foobar: Query<(&mut Direction, &Transform), With<Foobar>>,
     query_barfoo: Query<&Transform, With<Barfoo>>,
 ) {
-    if let Ok(barfoo_transform) = query_barfoo.get_single() {
-        for (mut velocity, foobar_transform) in query_foobar.iter_mut() {
-            let direction =
-                (barfoo_transform.translation - foobar_transform.translation).truncate();
-            if direction.length() != 0.0 {
-                let normalized_direction = direction.normalize();
-                let speed = velocity.0.length();
-                velocity.0 = normalized_direction * speed;
-            }
+    if let Some(barfoo_transform) = query_barfoo.iter().next() {
+        for (mut direction, foobar_transform) in query_foobar.iter_mut() {
+            direction.0 = (barfoo_transform.translation - foobar_transform.translation).truncate();
+        }
+    }
+}
+
+fn spawn_barfoo_on_click(
+    commands: Commands,
+    windows: Query<&Window>,
+    camera_q: Query<(&Camera, &GlobalTransform)>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+) {
+    let window = windows.single();
+    let (camera, camera_transform) = camera_q.single();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor).ok())
+    {
+        if mouse_button_input.just_pressed(MouseButton::Left) {
+            spawn_barfoo(commands, world_position);
         }
     }
 }
@@ -62,6 +78,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (update_position, update_velocity))
+        .add_systems(FixedUpdate, (update_position, update_direction))
+        .add_systems(FixedUpdate, spawn_barfoo_on_click)
         .run();
 }
